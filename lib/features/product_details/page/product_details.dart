548d7a991ee429276016df1a8a6be76dbb8aefa6
Mohamed hassan <67841458+MohamedHassan1311@users.noreply.github.com>
2023-07-21 14:51:26 +0300

    updates

--- lib/features/product_details/page/product_details.dart
+++ lib/features/product_details/page/product_details.dart
@@ -1,3 +1,4 @@
+import 'package:casa/components/shimmer/custom_shimmer.dart';
 import 'package:flutter/material.dart';
 import 'package:casa/app/core/utils/extensions.dart';
 import 'package:casa/components/custom_network_image.dart';
@@ -26,8 +27,7 @@ class _ProductDetailsState extends State<ProductDetails> {
   void initState() {
     Future.delayed(Duration.zero, () {
       Provider.of<ProductDetailsProvider>(context, listen: false).model = null;
-      Provider.of<ProductDetailsProvider>(context, listen: false)
-          .geDetails(widget.id);
+      Provider.of<ProductDetailsProvider>(context, listen: false).geDetails(widget.id);
     });
     super.initState();
   }
@@ -39,46 +39,117 @@ class _ProductDetailsState extends State<ProductDetails> {
         top: false,
         child: Consumer<ProductDetailsProvider>(
             builder: (context, provider, child) {
-          return provider.isLoading
-              ? SizedBox()
-              : provider.model != null
-                  ? Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Stack(
-                          children: [
-                            const CustomAppBar(),
-                            CustomNetworkImage.containerNewWorkImage(
-                                image: provider.model?.image ?? "",
-                                width: context.width,
-                                fit: BoxFit.fitWidth,
-                                height: 300.h,
-                                radius: 0),
-                          ],
-                        ),
-                        Expanded(
-                          child: ProductDetailsWidget(
-                            placeItem: provider.model!,
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.symmetric(
-                            vertical: Dimensions.PADDING_SIZE_DEFAULT.h,
-                              horizontal: Dimensions.PADDING_SIZE_DEFAULT.w),
-                          child: CustomButton(
-                            text: "حجز موعد",
-                            svgIcon: SvgImages.arrowLeftIcon,
-                            iconColor: Styles.WHITE_COLOR,
-                            onTap: () {},
-                          ),
-                        ),
-                      ],
-                    )
-                  : const EmptyState(
-                      emptyHeight: 200,
-                      imgHeight: 110,
-                    );
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                children: [
+                  const CustomAppBar(),
+                  provider.isLoading
+                      ? CustomShimmerContainer(
+                          height: 300.h,
+                          width: context.width,
+                        )
+                      : Visibility(
+                    visible: !provider.isLoading && provider.model != null,
+                    child: CustomNetworkImage.containerNewWorkImage(
+                            image: provider.model?.image ?? "",
+                            width: context.width,
+                            fit: BoxFit.fitWidth,
+                            height: 300.h,
+                            radius: 0),
+                      ),
+                ],
+              ),
+              provider.isLoading
+                  ? const _ProductDetailsWidgetShimmer()
+                  : provider.model != null
+                      ? ProductDetailsWidget(
+                          productItem: provider.model!,
+                        )
+                      : const EmptyState(),
+              Visibility(
+                visible: !provider.isLoading && provider.model != null,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                      vertical: Dimensions.PADDING_SIZE_DEFAULT.h,
+                      horizontal: Dimensions.PADDING_SIZE_DEFAULT.w),
+                  child: CustomButton(
+                    text: "حجز موعد",
+                    svgIcon: SvgImages.arrowLeftIcon,
+                    iconColor: Styles.WHITE_COLOR,
+                    onTap: () {},
+                  ),
+                ),
+              ),
+            ],
+          );
         }),
+      ),
+    );
+  }
+}
+
+class _ProductDetailsWidgetShimmer extends StatelessWidget {
+  const _ProductDetailsWidgetShimmer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: Dimensions.PADDING_SIZE_DEFAULT.w,
+                vertical: Dimensions.PADDING_SIZE_SMALL.h),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const CustomShimmerContainer(
+                  width: 150,
+                  height: 30,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 6.h),
+                  child: const CustomShimmerContainer(
+                    width: 150,
+                    height: 30,
+                  ),
+                ),
+                ...List.generate(
+                  5,
+                  (index) => Padding(
+                    padding: EdgeInsets.symmetric(vertical: 2.h),
+                    child: CustomShimmerContainer(
+                      width: context.width,
+                      height: 15,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 184.h,
+            child: Center(
+              child: ListView.builder(
+                physics: const BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                itemCount: 5,
+                itemBuilder: (c, index) => Padding(
+                  padding:
+                      EdgeInsets.only(right: index == 0 ? 24.w : 0, left: 12.w),
+                  child: CustomShimmerContainer(
+                    width: 100.w,
+                    height: 150.h,
+                    radius: 8,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
