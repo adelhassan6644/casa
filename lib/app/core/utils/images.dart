b3e1d80c17bf7f52a74d43a8b54a8130b2086015
Mohamed hassan <67841458+MohamedHassan1311@users.noreply.github.com>
2023-08-07 00:06:16 +0300

    first sprint

--- lib/app/core/utils/images.dart
+++ lib/app/core/utils/images.dart
@@ -5,6 +5,7 @@ class Images {
   static const String profileBGImage = 'assets/images/profile_bg.png';
   static const String developerImage = 'assets/images/developer_image.png';
   static const String check = 'assets/images/check_circle.png';
+  static const String success = 'assets/images/success.png';
   static const String contactPhone = 'assets/images/contact_phone.png';
   static const String cancelCircle = 'assets/images/cancel_circle.png';
   static const String whatsApp = 'assets/images/whatsapp.png';
